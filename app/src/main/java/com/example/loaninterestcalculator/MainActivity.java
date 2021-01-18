6d2559185c5bacce7b14afeaf6411ec0456e696f
bhargavab19 <bhargav.bhupatkar@gmail.com>
2021-01-18 19:25:47 +0530

    Initially uploading all the files

---
+++ app/src/main/java/com/example/loaninterestcalculator/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.loaninterestcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity{
+
+    String str_pymntFrequency;
+    static int freq;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnCalc=(Button) findViewById(R.id.button_calculate);
+
+        Spinner pymntFrequency=(Spinner) findViewById(R.id.spinner_repaymentType);
+        pymntFrequency.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                str_pymntFrequency =parent.getItemAtPosition(position).toString();
+                switch (position){
+                    case 0:
+                        freq=12;
+                        break;
+
+                    case 1:
+                        freq=4;
+                        break;
+
+                    case 2:
+                        freq=2;
+                        break;
+
+                    case 3:
+                        freq=1;
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        btnCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText eTextLoanAmount=(EditText) findViewById(R.id.editText_loanAmount);
+                EditText eTextInterestRate=(EditText) findViewById(R.id.editText_interestRate);
+                EditText eTextPeriod=(EditText) findViewById(R.id.editText_period);
+                TextView emiAmount=(TextView) findViewById(R.id.textView_emiAmount);
+                TextView effROI=(TextView) findViewById(R.id.textView_effROI);
+
+                double pv= Double.parseDouble(eTextLoanAmount.getText().toString());
+                double r= Double.parseDouble(eTextInterestRate.getText().toString())/(freq*100);
+                int per=Integer.parseInt(eTextPeriod.getText().toString())*freq;
+
+                Double emiCalc=(pv*r*Math.pow(1+r,per))/(Math.pow(1+r,per)-1);
+                Double effROICalc=(emiCalc*per-pv)/pv*100;
+                DecimalFormat formatVal=new DecimalFormat("#,###.##");
+
+                emiAmount.setText("\u20B9" + formatVal.format(emiCalc));
+                effROI.setText(formatVal.format(effROICalc)+"%");
+            }
+        });
+    }
+}
